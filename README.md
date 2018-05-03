1352ca229e431b9ac9842c78ba2cb07a63286fa8
Gryphon Myers <gryphon.myers@2k.com>
2018-05-03 14:42:02 +0800

    add files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # transitionend-promise
 Promisifies the transitionend js event for CSS transitions
+
+## Installation
+
+```
+npm install transitionend-promise
+```
+
+## Usage
+
+```css
+    #foo {
+        transition: 1s transform;
+        transform: rotate(0deg);
+    }
+    
+    .do-a-barrel-roll {
+        transform: rotate(360deg);
+    }
+```
+
+```javascript
+    const transitionEnd = require('transitionend-promise);
+
+    var myDOMEl = document.querySelector('#foo');
+
+    myDOMEl.classList.add('do-a-barrel-roll');
+
+    transitionPromise(myDOMEl, 'transform')
+        .then(evt => {
+            console.log('We did a barrel roll and I have the origin transitionend event object to prove it.', evt);
+        });
+```
---
+++ src/transitionend-promise.js
@@ -0,0 +1,34 @@
+var vendorPrefix = require('vendor-prefix').dash;
+
+function parseDuration(val) {
+    return val.slice(-1) === 's' ? Number(val.slice(0, -1)) * 1000 : Number(val)
+}
+
+module.exports = function(el, prop) {
+    if (el.parentNode && vendorPrefix('transition')) {
+        prop = prop ? vendorPrefix(prop) : null;
+        var style = getComputedStyle(el);
+        var durations = style.transitionDuration.split(',');
+        var properties = style.transitionProperty.split(',');
+
+        var timeout = properties.reduce((final, curr, ii) => {
+            return curr === 'all' ? 
+                Math.max(parseDuration(durations[ii]), final)
+            : (curr && (!prop || curr === prop)) ? 
+                Math.max(parseDuration(durations[ii]), final)
+            : final;
+        }, 0);
+
+        return new Promise(function(resolve, reject){
+            el.addEventListener('transitionend', function onTransitionEnd(e){
+                if (e.target == el && (!prop || (Array.isArray(prop) ? prop.indexOf(e.propertyName) > -1 : prop == e.propertyName))) {
+                    resolve(e);
+                    el.removeEventListener('transitionend', onTransitionEnd);
+                }
+            });
+            setTimeout(resolve, timeout);
+        });
+    } else {
+        return Promise.resolve(null);
+    }
+}
